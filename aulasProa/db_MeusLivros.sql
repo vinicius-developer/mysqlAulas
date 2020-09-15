ad6c6c664726b74ae0b78a5506499582348c1047
Vinicius de Paula <viniciusilvav1@hotmail.com>
2020-09-14 23:34:23 -0300

    banco atividade meus livros

--- aulasProa/db_MeusLivros.sql
+++ aulasProa/db_MeusLivros.sql
@@ -7,17 +7,28 @@ create table Livros(
     ISBN varchar(13) not null unique,
     assunto_livro varchar(30) not null,
 	nome_editora_livro varchar(80) not null,
-    preco_livro float(6,2) not null,
+    preco_livro float not null,
     data_publicacao date, 
     numero_paginas int not null,
     primary key(id_livro)
 ) auto_increment = 100;
 
+/* Neste exemplo criamos uma fk */
+
 create table Autor(
-	id_livro bigint not null,
+	fk_id_livro bigint not null,
     nome_autor varchar(30) not null,
     sobreome_autor varchar(80) not null,
-    constraint autores_livros foreign key (id_livro) references Livros (id_livros)
+	#foreign key(nome do campo que vai ser a FK) references NomeDaTabelaMae(nome Do campo na tabela mãe)
+    foreign key(fk_id_livro) references Livros(id_livro)
 );
 
+alter table Autor
+add foreign key(id_livro)
+references Livros(id_livro);
 
+# drop table Livros
+drop table Autor 
+
+
+ 
